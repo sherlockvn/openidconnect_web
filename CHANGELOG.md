491ed3d7ab0e665007b8c27462157aafb4fc452b
sherlockvn <quangkhai28999@gmail.com>
2022-05-04 11:15:22 +0700

    openidconnect_web

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+## [1.0.19] - December 9th, 2021
+
+- Update to new interface and fix errors.
+
+## [1.0.12] - November 29th, 2021
+
+- Update dependencies
+
+## [1.0.11] - September 10, 2021
+
+- Updated to Dart 2.14
+- Added linting
+
+## [1.0.10]
+
+- Add the endorsement directive in the pubspec.yaml
+
+## 1.0.9
+
+- Implement redirect loop functionality in addition to the default web popup.
+
+## 1.0.7
+
+- Fix readme instructions
+
+## 1.0.6
+
+- Initial Release
---
+++ lib/openidconnect_web.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+// In order to *not* need this ignore, consider extracting the "web" version
+// of your plugin as a separate package, instead of inlining it in the same
+// package as the core of your plugin.
+// ignore: avoid_web_libraries_in_flutter
+import 'package:flutter/widgets.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:openidconnect_platform_interface/openidconnect_platform_interface.dart';
+import "dart:html" as html;
+
+/// A web implementation of the OpenidconnectWeb plugin.
+class OpenIdConnectWeb extends OpenIdConnectPlatform {
+  static void registerWith(Registrar registrar) {
+    OpenIdConnectPlatform.instance = OpenIdConnectWeb();
+  }
+
+  @override
+  Future<String?> authorizeInteractive({
+    BuildContext? context,
+    required String title,
+    required String authorizationUrl,
+    required String redirectUrl,
+    required int popupWidth,
+    required int popupHeight,
+    bool useWebRedirectLoop = false,
+  }) async {
+    if (useWebRedirectLoop) {
+      const AUTH_DESTINATION_KEY = "openidconnect_auth_destination_url";
+      html.window.sessionStorage[AUTH_DESTINATION_KEY] =
+          html.window.location.toString();
+      html.window.location.assign(authorizationUrl);
+      return Future<String?>.value(null);
+    }
+
+    final top = (html.window.outerHeight - popupHeight) / 2 +
+        (html.window.screen?.available.top ?? 0);
+    final left = (html.window.outerWidth - popupWidth) / 2 +
+        (html.window.screen?.available.left ?? 0);
+
+    var options =
+        'width=${popupWidth},height=${popupHeight},toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no&top=$top,left=$left';
+
+    final child = html.window.open(
+      authorizationUrl,
+      "open_id_connect_authentication",
+      options,
+    );
+
+    final c = new Completer<String>();
+    html.window.onMessage.first.then((event) {
+      final url = event.data.toString();
+      c.complete(url);
+      child.close();
+    });
+
+    return c.future;
+  }
+
+  @override
+  Future<String?> processStartup() async {
+    const AUTH_RESPONSE_KEY = "openidconnect_auth_response_info";
+
+    final url = html.window.sessionStorage[AUTH_RESPONSE_KEY];
+    html.window.sessionStorage.remove(AUTH_RESPONSE_KEY);
+
+    return url;
+  }
+}
